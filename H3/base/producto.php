4313bda1c2fa81455e32b8b2577e8abf1999a50c
noehm05 <noelia.huanca@uab.edu.bo>
2026-09-12 17:20:46 -0400

    Update producto.php

--- H3/base/producto.php
+++ H3/base/producto.php
@@ -1,1 +1,15 @@
-h
+<?php
+class Producto
+{
+    public $idProducto;
+    public $nombre;
+    public $precio;
+    public $stock;
+
+    public function registrarProducto()
+    {
+        echo "Producto registrado correctamente.";
+    }
+}
+
+?>
