7138558b17e3a2ab9efbbd0f5706cf27d0c5d27a
noehm05 <noelia.huanca@uab.edu.bo>
2026-09-12 17:21:21 -0400

    Create categoria.php

---
+++ H3/base/categoria.php
@@ -0,0 +1,13 @@
+<?php
+class Categoria
+{
+    public $idCategoria;
+    public $nombre;
+
+    public function registrarCategoria()
+    {
+        echo "Categoría registrada correctamente.";
+    }
+}
+
+?>
