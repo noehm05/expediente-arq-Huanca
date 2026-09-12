7af262fa45e18ff367fa9bcae47f982dbd1064be
noehm05 <noelia.huanca@uab.edu.bo>
2026-09-12 17:23:10 -0400

    Create venta.php

---
+++ H3/base/venta.php
@@ -0,0 +1,14 @@
+<?php
+class Venta
+{
+    public $idVenta;
+    public $fecha;
+    public $total;
+
+    public function calcularTotal()
+    {
+        echo "Calculando total de la venta.";
+    }
+}
+
+?>
